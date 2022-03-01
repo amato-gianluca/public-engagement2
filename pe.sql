b16997501a4655968df238f3b74ea4b111e41564
Gianluca Amato <gianluca.amato.74@gmail.com>
2022-03-02 00:55:21 +0100

    Refine type of fields in database.

--- pe.sql
+++ pe.sql
@@ -1,23 +1,23 @@
 CREATE TABLE `keywords` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `keyword` varchar(255) DEFAULT NULL,
-  `lang` char(2) DEFAULT NULL,
+  `keyword` varchar(255) NOT NULL,
+  `lang` char(2) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `researchers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` char(6) DEFAULT NULL,
-  `keywords_en` text DEFAULT NULL,
-  `interests_en` text DEFAULT NULL,
-  `demerging_en` text DEFAULT NULL,
-  `awards_en` text DEFAULT NULL,
-  `curriculum_en` text DEFAULT NULL,
-  `keywords_it` text DEFAULT NULL,
-  `interests_it` text DEFAULT NULL,
-  `demerging_it` text DEFAULT NULL,
-  `awards_it` text DEFAULT NULL,
-  `curriculum_it` text DEFAULT NULL,
+  `username` char(6) NOT NULL UNIQUE,
+  `keywords_en` text NOT NULL DEFAULT '',
+  `interests_en` text NOT NULL DEFAULT '',
+  `demerging_en` text NOT NULL DEFAULT '',
+  `awards_en` text NOT NULL DEFAULT '',
+  `curriculum_en` text NOT NULL DEFAULT '',
+  `keywords_it` text NOT NULL DEFAULT '',
+  `interests_it` text NOT NULL DEFAULT '',
+  `demerging_it` text NOT NULL DEFAULT '',
+  `awards_it` text NOT NULL DEFAULT '',
+  `curriculum_it` text NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   FULLTEXT KEY `fidx` (`keywords_en`,`interests_en`,`demerging_en`,`awards_en`,`curriculum_en`,`keywords_it`,`interests_it`,`demerging_it`,`awards_it`,`curriculum_it`)
 );
@@ -25,6 +25,6 @@ CREATE TABLE `researchers` (
 CREATE TABLE `researcher_keywords` (
   `id_researcher` int(11) NOT NULL,
   `id_keyword` int(11) NOT NULL,
-  `pos` tinyint(3) unsigned DEFAULT NULL,
+  `pos` tinyint(3) unsigned NOT NULL,
   PRIMARY KEY (`id_researcher`,`id_keyword`)
 );
