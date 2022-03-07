00de23f2ce72e90489ab6027fdb76339c647de4b
Gianluca Amato <gianluca.amato.74@gmail.com>
2022-03-07 10:37:13 +0100

    Credits updated.

--- credits.php
+++ credits.php
@@ -7,15 +7,15 @@ require_once 'templates/header.php';
                 </div>
 
                 <p>
-                    Questa applicazione web è stata sviluppata da <a href="https://www.sci.unich.it/~amato/">Gianluca Amato</a> (professore associato di Logica Matematica
-                    al Dipartimento di Economia) su un'idea originale di <a href="https://dea.unich.it/home-venditti-michelina-2131">Michelina Venditti</a>
-                    (professore ordinario di Economia Aziendale al Dipartimento di Economia Aziendale).
+                    Questa applicazione web è stata sviluppata da <a href="https://www.sci.unich.it/~amato/">Gianluca Amato</a>, che ne cura installazione, manutenzione e aggiornamenti.
+                    Il software nasce da un'idea di <a href="https://dea.unich.it/home-venditti-michelina-2131">Michelina Venditti</a>.
                 </p>
                 <p>
-                    Il programma è attualmente installata sui server per le attività di teledidattica. Si ringrazia il team per lo sviluppo dei siti web
-                    avanzati di dipartimenti e corsi di laurea (<a href="https://cleii.unich.it/home-di-nardo-di-maio-simone-177">Simone Di Nardo Di Maio</a>,
+                    Il programma è attualmente installato sui server per le attività di teledidattica. Si ringrazia il team per lo sviluppo dei nuovi siti web
+                    dei dipartimenti e dei corsi di laurea (<a href="https://cleii.unich.it/home-di-nardo-di-maio-simone-177">Simone Di Nardo Di Maio</a>,
                     <a href="https://www.dec.unich.it/home-fioravanti-fabio-70">Fabio Fioravanti</a>, <a href="https://www.dec.unich.it/home-moscardelli-luca-61">Luca Moscardelli</a>)
-                    per i template HTML e CSS necessari all'integrazione grafica con gli altri siti di ateneo.
+                    per i template HTML e CSS necessari all'integrazione grafica con gli altri siti di ateneo e <a href="https://www.dec.unich.it/home-scozzari-francesca-172">Francesca Scozzari</a>
+                    per il supporto all'accesso ai dati utilizzati dall'applicazione.
                 </p>
 <?php
 require_once("templates/footer.php");
